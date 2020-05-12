f93b9412c52545284535acb1a9f91469f72c7fff
atoder <alexandert@gmail.com>
2020-05-11 19:51:36 -0700

    fixing double quote to single quote

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./containers/App";
-import * as serviceWorker from "./serviceWorker";
-import "tachyons";
-import { searchFriends } from "./reducers";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { searchFriends } from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import {createLogger} from 'redux-logger';
 
 const store = createStore(searchFriends);
 
